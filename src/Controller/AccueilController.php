b42abe9bf73359cadcd14825ab33bcd7464d65c5
EnzOméga <zeroghysbrecht15@gmail.com>
2025-09-12 10:27:11 +0200

    Phase 3 : utilisation de Twig".

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,16 +4,17 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Description of AccueilController
  *
  * @author enzog
  */
-class AccueilController {
+class AccueilController extends AbstractController{
     
     #[Route('/', name: 'accueil')]
     public function index(): Response {
-        return new Response('Hello world !');
+        return $this->render("pages/accueil.html.twig");
     }
 }
