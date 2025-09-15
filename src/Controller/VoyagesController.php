18dcab2238927f26c68b856ebfde0a6c87e36bc4
EnzOméga <zeroghysbrecht15@gmail.com>
2025-09-15 11:39:48 +0200

    Phase 8 : ORM (tri des enregistrements).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -16,9 +16,17 @@ class VoyagesController extends AbstractController{
     
     #[Route('/voyages', name: 'voyages')]
     public function index(): Response {
-        $visite = $this->repository->findAll();
+        $visite = $this->repository->findAllOrderBy('datecreation', 'DESC');
         return $this->render("pages/voyages.html.twig", [
             'visites' => $visite
+        ]);
+    }
+    
+    #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.sort')]
+    public function sort($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
         ]);
     }
     
